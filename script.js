491d8614ec72e5b58337c7632f6ef055083f0497
Natallia Yurcha <natallia.yurcha@gmail.com>
2021-01-13 23:38:59 +0300

    Bug fixes and improvmend. Add animation to elements. Add favicon. Add page titles.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-var carousels = document.querySelectorAll('#carousels .carousel');
-var currentCarousel = 0;
-var carouselInterval = setInterval(nextCarousel,4000); /* Интервал между картинками */
+var carousels = document.querySelectorAll('#carousels .carousel'); /*получаем все слайды из контейнера*/
+var currentCarousel = 0; /*создаем переменную для текущего слайда*/
+var carouselInterval = setInterval(nextCarousel, 4000); /*Интервал между картинками */
 
-function nextCarousel(){
-carousels[currentCarousel].className = 'carousel';
-currentCarousel = (currentCarousel+1)%carousels.length;
-carousels[currentCarousel].className = 'carousel demonstration';
+function nextCarousel() {
+    carousels[currentCarousel].className = 'carousel'; /*меняем класс для текущего слайда, чтобы спрятать его*/
+    currentCarousel = (currentCarousel + 1) % carousels.length; /*добавляем класс к текущему слайду. Мы используем оператор % на случай, если это был последний слайд, чтобы вернуться к первому*/
+    carousels[currentCarousel].className = 'carousel demonstration'; /*После получения индекса слайда мы меняем класс и показываем новый*/
 }
 
